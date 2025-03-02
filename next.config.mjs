28b8907f6b7efe94444637c4444ffe4c5706bab8
James Golebie <labber@duck.com>
2025-03-02 11:55:24 -0500

    Remove: Additional OpenAI references and configurations

--- next.config.mjs
+++ next.config.mjs
@@ -10,27 +10,11 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "replicate.com",
-      },
-      {
-        protocol: "https",
-        hostname: "replicate.delivery",
-      },
-      {
-        protocol: "https",
         hostname: "firebasestorage.googleapis.com",
       },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.openai.com/:path*",
-      },
-    ];
   },
 };
 
